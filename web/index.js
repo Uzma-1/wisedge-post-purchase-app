02079813b2e67257815c83f98fe7a30f2a0d105c
Uzma-1 <um4998841@gmail.com>
2023-12-27 13:13:08 +0530

    update index.js file

--- web/index.js
+++ web/index.js
@@ -49,22 +49,16 @@ app.post(
 
 // If you are adding routes outside of the /api path, remember to
 // also add a proxy rule for them in web/frontend/vite.config.js
-
-app.use(cors({
-  origin: 'https://cdn.shopify.com',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-}));
-
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://cdn.shopify.com'); // Update with your actual origin
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Origin', 'https://cdn.shopify.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
+app.use(cors());
 
 
 
