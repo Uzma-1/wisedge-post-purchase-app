427ebef65860b35ecb37630769f978c33a7d7a4d
Uzma-1 <um4998841@gmail.com>
2023-12-27 16:47:04 +0530

    update index.js file

--- web/index.js
+++ web/index.js
@@ -52,14 +52,16 @@ app.post(
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://cdn.shopify.com');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}));
 
 
 
