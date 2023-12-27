5454ae246d0ef76abe1d314212ab0f705cebe96c
Uzma-1 <um4998841@gmail.com>
2023-12-27 16:46:28 +0530

    update index.js file

--- web/index.js
+++ web/index.js
@@ -55,13 +55,8 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://cdn.shopify.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  // Enable preflight requests
-  if (req.method === 'OPTIONS') {
-    res.status(200).send();
-  } else {
-    next();
-  }
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
 });
 
 app.use(cors());
