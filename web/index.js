77fb5972fd8169c85d30c253e091d1e073a8fbcc
Uzma-1 <um4998841@gmail.com>
2023-12-29 18:02:09 +0530

    update code'

--- web/index.js
+++ web/index.js
@@ -252,7 +252,7 @@ app.post("/api/sign-changeset", cors(), async (req, res) => {
           var lastOrder = customer_detail[lastOrderIndex];
           currentOrderId = lastOrder?.[0]?.id;
 
-          console.log('currentOrderId???', lastOrder);
+          console.log('lastOrder???', lastOrder?.[0]);
 
           // Define the tags to add for the current order
           const tags = tagsToAdd;
